1d20cb566a11af213fe62ed4daf51cde93697674
Donovan Keohane <pinoycoding@gmail.com>
2016-04-08 20:13:39 -0400

    save

--- chat/server.js
+++ chat/server.js
@@ -1,13 +1,12 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
-
-var cache = {};
+var http  = require('http')
+  , fs    = require('fs')
+  , path  = require('path')
+  , mime  = require('mime')
+  , cache = {};
 
 function send404(response) {
   response.writeHead(404, {'Content-Type': 'text/plain'});
-  response.write('Error 404: Resource not found.')
+  response.write('Error 404: resource not found.');
   response.end();
 }
 
@@ -24,34 +23,36 @@ function serveStatic(response, cache, absPath) {
     sendFile(response, absPath, cache[absPath]);
   } else {
     fs.exists(absPath, function(exists) {
-    if (exists) {
-      fs.readFile(absPath, function(err, data) {
-      if (err) {
-        send404(response);
+      if (exists) {
+        fs.readFile(absPath, function(err, data) {
+          if (err) {
+            send404(response);
+          } else {
+            cache[absPath] = data;
+            sendFile(response, absPath, data);
+          }
+        });
       } else {
-        cache[absPath] = data;
-        sendFile(response, absPath, data);
+        send404(response);
       }
-    });
-    } else {
-      send404(response);
-    }
     });
   }
 }
 
 var server = http.createServer(function(request, response) {
   var filePath = false;
-
   if (request.url == '/') {
     filePath = 'public/index.html';
   } else {
-    file = 'public' + request.url;
+    filePath = 'public' + request.url;
   }
   var absPath = './' + filePath;
-  serveStatic(request, cache, absPath);
+  serveStatic(response, cache, absPath);
 });
 
 server.listen(3000, function() {
   console.log("Server listening on port 3000.");
 });
+
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
