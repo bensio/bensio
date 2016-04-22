6ddb28cd316ca01395839bcd8ea765e29dd324d9
Benjamin Muhlestein <betropper@gmail.com>
2016-04-22 19:42:09 -0400

    save

--- js/game.js
+++ js/game.js
@@ -54,7 +54,7 @@ function create() {
   if (localStorage && localStorage.getItem('money')) {
     money = parseInt(localStorage.getItem('money'));
   }
-  if (localStorage && localStorage.getItem('name')) { 
+  if (localStorage.getItem('name')) { 
     playerName = localStorage.getItem('name');
   } else {
     playerName = prompt("Welcome to Bensio! Enter a name for this computer here:", "Name");    
