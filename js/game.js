1ae564c0705639d8dca2b4745bb0477d4311ff7e
Benjamin Muhlestein <betropper@gmail.com>
2016-04-25 20:52:48 -0400

    save

--- js/game.js
+++ js/game.js
@@ -77,7 +77,7 @@ function create() {
   blue = blocks.create(200, 150, 'blue');
   red = blocks.create(200, 744, 'red');
   green = blocks.create(1008, 150, 'green');
-  orange = blocks.create(1008, 744, 'orange');
+i  orange = blocks.create(1008, 744, 'orange');
   sock = new WebSocket("ws://" + ip + ":8000/ws");
   sock.onopen = function() {
             var currency = JSON.stringify({
