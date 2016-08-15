d3666f46eebd2c8f8611ff94a3d78a65a9785942
Benjamin Muhlestein <betropper@gmail.com>
2016-08-15 18:57:30 -0400

    save

--- js/game.js
+++ js/game.js
@@ -317,7 +317,7 @@ function checkOutOfBounds(circle) {
             circle.body.setCollisionGroup(purpleCircleCollisionGroup);
             circle.body.collides(blockCollisionGroup);
             circle.circleShape.sensor = true;
-            circle.body.onBeginContact.add(hitBlock, circle.circleShape);
+            circle.body.onBeginContact.add(hitBlock, circle);
             circle.body.kinematic = true;
           }
           purpleCircle = purpleCircles.create(game.world.centerX - 100, game.world.centerY+405, 'purplecircle');
@@ -389,9 +389,7 @@ function hitBlock (body,bodyB,shapeA,shapeB,equation) {
       body.sprite.destroy();
     }
 
-    else if (body) {
-      console.log(body);
-      console.log("YOU DID IT");
+    else if (body && body.body && body.body.sprite.key == "purplecircle") {
       body.sprite.alpha = 1;
       body.sprite.health += 2;
   } 
