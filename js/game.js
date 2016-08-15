e27004bf27dcddae1a7114023771b68d5016da7a
Benjamin Muhlestein <betropper@gmail.com>
2016-08-15 19:52:57 -0400

    save

--- js/game.js
+++ js/game.js
@@ -652,7 +652,7 @@ function update() {
               purpleCircle.scale.y += .02;
               purpleCircle.alpha -= .002;
               purpleCircle.circleShape = (36 * purpleCircle.scale.y);        
-              console.log(purpleCircle;
+              console.log(purpleCircle);
             } 
           }
         }, this);
